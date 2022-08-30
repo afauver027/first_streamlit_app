8d8e4b5e62fadffbeedf705f3451c8ebca182218
afauver027 <111078288+afauver027@users.noreply.github.com>
2022-08-30 09:05:38 -0400

    deleted code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 
 # Create table for fruityvice API
